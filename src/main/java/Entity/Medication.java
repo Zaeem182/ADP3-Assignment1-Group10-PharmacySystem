8a8fdaace225bbbae154ee3574160d85d2b48a74
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2022-08-07 19:48:19 +0200

    Update Medication.java

--- src/main/java/Entity/Medication.java
+++ src/main/java/Entity/Medication.java
@@ -2,7 +2,7 @@ package Entity;
 /*Medication.java
 Entity for the Medication
 Author : Ilyaas Davids (219466424)
-Date : 10 April 2022*/
+Date : 7 August 2022*/
 public class Medication {
     private String medId ;
     private String suppId ;
